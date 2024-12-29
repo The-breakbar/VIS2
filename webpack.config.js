a60fba86bbc66f46f96cc345d9e545b56f65e4da
Sebastian K <sebastian76567@gmail.com>
2024-12-29 13:40:12 +0100

    updated packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: "development",
-	entry: "./src/index.ts",
-	devtool: "inline-source-map",
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html"
-		})
-	],
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true
-	}
+    mode: 'development',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    target: 'node',
 };

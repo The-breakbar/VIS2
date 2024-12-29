444b7396f6d5f374364d958cabb3ec3815fd9c00
The-breakbar <breakbar.email@gmail.com>
2024-12-29 21:43:41 +0100

    added formatting config, fixed csv loading, added csv data loader to webpack

--- src/index.ts
+++ src/index.ts
@@ -3,54 +3,36 @@ import { readCSVasJSON } from './space_tesselation/readCSV';
 import { createTrajectoriesFromCSV } from './space_tesselation/trajectories';
 import { extractCharisticPointsFromAllTrajectories } from './space_tesselation/characteristicPoints';
 import {
-    groupingCharacteristicPointsInSpace,
-    optimizeGroupsInRespectToPointDensity,
+	groupingCharacteristicPointsInSpace,
+	optimizeGroupsInRespectToPointDensity,
 } from './space_tesselation/groupCharacteristicPoints';
 import { extractMovementIntoOneArray, movementToPoints } from './tools';
 
-// Declare the chart dimensions and margins.
-const width = 640;
-const height = 400;
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
-
-// Declare the x (horizontal position) scale.
-const x = d3
-    .scaleUtc()
-    .domain([new Date('2023-01-01'), new Date('2024-01-01')])
-    .range([marginLeft, width - marginRight]);
-
-// Declare the y (vertical position) scale.
-const y = d3
-    .scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
-
-// Create the SVG container.
-const svg = d3.create('svg').attr('width', width).attr('height', height);
-
-// Add the x-axis.
-svg.append('g')
-    .attr('transform', `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
-
-// Add the y-axis.
-svg.append('g').attr('transform', `translate(${marginLeft},0)`).call(d3.axisLeft(y));
-
-// Append the SVG element.
-document.body.appendChild(svg.node() as Node);
-
 // Load the CSV file and compute the data.
-function loadAndComputeCSV(filePath: string) {
-    const csv = readCSVasJSON(filePath);
-    const trajectories = createTrajectoriesFromCSV(csv);
-    const characteristicPoints = extractCharisticPointsFromAllTrajectories(trajectories);
-    const movements = extractMovementIntoOneArray(characteristicPoints);
-    const points = movementToPoints(movements);
-    const groupedOutput = groupingCharacteristicPointsInSpace(points, 0.0001);
-    const Groups = groupedOutput[0];
-    const Grid = groupedOutput[1];
-    const optimizedGroups = optimizeGroupsInRespectToPointDensity(points, Groups, Grid);
+async function loadAndComputeCSV(filePath: string) {
+	const csv = await readCSVasJSON(filePath);
+	const trajectories = createTrajectoriesFromCSV(csv);
+	const characteristicPoints =
+		extractCharisticPointsFromAllTrajectories(trajectories);
+	const movements = extractMovementIntoOneArray(characteristicPoints);
+	const points = movementToPoints(movements);
+	const groupedOutput = groupingCharacteristicPointsInSpace(points, 0.0001);
+	const Groups = groupedOutput[0];
+	const Grid = groupedOutput[1];
+	const optimizedGroups = optimizeGroupsInRespectToPointDensity(
+		points,
+		Groups,
+		Grid
+	);
+
+	console.log(`CSV file loaded and computed: ${filePath}`);
+	console.log(`Trajectories: ${trajectories.length}`);
+	console.log(`Characteristic points: ${characteristicPoints.length}`);
+	console.log(`Movements: ${movements.length}`);
+	console.log(`Points: ${points.length}`);
+	console.log(`Groups: ${Groups.length}`);
+	console.log(`Grid: ${Grid.length}`);
+	console.log(`Optimized groups: ${optimizedGroups.length}`);
 }
+
+loadAndComputeCSV('./data/MilanoData.csv');
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    target: 'node',
+	mode: 'development',
+	entry: './src/index.ts',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.csv$/,
+				use: 'asset/resource'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new CopyPlugin({
+			patterns: [{ from: 'data', to: 'data' }]
+		})
+	],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
+	}
 };
